aa0fa4ae8e9fd1535ab1778708561ca6b0f63208
hcxxstl <hcxxstl@126.com>
2019-02-22 15:14:40 +0800

    updated bash file

--- guessinggame.sh
+++ guessinggame.sh
@@ -4,31 +4,40 @@
 # Written by @hcxxstl
 
 function main(){
-        if [[ $guess -gt $num ]]
-        then
-                echo "$guess is too high "
+
+while true
+do
+	echo "Please enter your guess:"
+	read guess
+
+	if [[ $guess =~ [^0-9] ]]
+	then
+		echo "$guess is not an Int"
+	elif [[ $guess -eq $num ]]
+	then
+		echo "Congratulations!"
+		echo "Your answer is correct!"
+		break
+	elif [[ $guess -gt $num ]]
+	then
+		echo "$guess is too high "
         else
-                echo "$guess is too low"
-        fi
-        echo "Please enter your guess:"
-        read guess
+       	        echo "$guess is too low"
+	fi
+done
+
 }
 
 echo "======= GAME START ======="
 echo "Guess the number of files"
 echo "in the current directory."
 
-num=$(ls|wc -l)
+# Count all files include hidden files and files in subfolders
+# num=$(find . -type f | wc -l)
+# Count regular files
+num=$(ls -l|grep "^-"|wc -l)
 guess=-1
 
-echo "Please enter your guess:"
-read guess
+main
 
-while [[ $guess -ne $num ]]
-do
-	main
-done
-
-echo "Congratulations!"
-echo "Your answer is correct!"
 echo "=======  GAME END  ======="
